c318f437000eca5f9cbd9865068c5a14143cb4f1
vagh3tt1 <ch2vaghetti@gmail.com>
2020-05-23 22:11:02 -0300

    C2 - Programa implementado

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,44 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Account;
+import model.exceptions.DomainException;
+
 public class Program {
 
 	public static void main(String[] args) {
 	
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner (System.in);
 
+		System.out.println("Enter account data");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		System.out.println("Holder: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.println("Initial Balance: ");
+		double balance = sc.nextDouble();
+		System.out.println("Withdrow limit: ");
+		double withDrawLimit = sc.nextDouble();
+		
+		Account acc = new Account(number, holder, balance, withDrawLimit);
+		
+		System.out.println();
+		System.out.println("Enter amount for withdraw: ");
+		double amount = sc.nextDouble();
+		
+		try {
+			acc.withdraw(amount);
+			System.out.println("New Balance: " + String.format("%.2f", acc.getBalance()));
+		}
+		catch (DomainException e) {
+			System.out.println("Withdraw error: " + e.getMessage());
+		}
+		sc.close();
 	}
 
 }
--- src/model/entities/Account.java
+++ src/model/entities/Account.java
@@ -1,5 +1,55 @@
 package model.entities;
 
-public class Account {
+import model.exceptions.DomainException;
 
+public class Account {
+	
+	private Integer number;
+	private String holder;
+	private Double balance;
+	private double withDrawLimit;
+	
+	public Account(Integer number, String holder, Double balance, double withDrawLimit) {
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+		this.withDrawLimit = withDrawLimit;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+
+	public double getWithDrawLimit() {
+		return withDrawLimit;
+	}
+
+	public void setWithDrawLimit(double withDrawLimit) {
+		this.withDrawLimit = withDrawLimit;
+	}
+	
+	public void withdraw (double amount) {
+		if (amount > withDrawLimit) {
+			throw new DomainException("The amount exceeds withdraw limit");
+		}
+		if (amount > balance ) {
+			throw new DomainException("Not enought balance");
+		}
+		balance -=amount;
+	}
+
+	
 }
